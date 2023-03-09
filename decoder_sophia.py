4551361a8fe9ca74d7c37b51c99629a27ba419d3
s-m-john <107650644+s-m-john@users.noreply.github.com>
2023-03-08 22:29:00 -0500

    Add files via upload

---
+++ decoder_sophia.py
@@ -0,0 +1,9 @@
+#SophiaJohn
+#Decoder
+
+elif choic e= =2:  # decoder
+# insert decoder
+pw_enc = str(input('Please enter your password to decode: '))
+decoded_pw = decode(pw_enc)
+print(decoded_pw)
+print('Your password has been decoded and is:', decoded_pw)
